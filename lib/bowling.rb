3a262def7d59a4c9edf7ff1d7bddf0c141f11d2e
natsu-cat <akisancat.natsu@gmail.com>
2020-02-21 04:07:45 +0900

    bowlingプログラムのテスト項目2check

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,9 +1,28 @@
 def bowling(scores)
   sum = 0
-  scores.map do |flame|
-    flame.map do |score|
-      sum = sum + score
+  check = Array.new(10, [0, 0])
+  scores.map.with_index do |score, index|
+    sum += score[0] + score[1]
+
+    if score[0] == 10
+      check[index, 0] = 2
+    elsif score[0] + score[1] == 10
+      check[index, 1] = 1
     end
   end
+
+  check.map.with_index do |n,index|
+    if n.class == Integer
+      sum += scores[index + 1][0 ]
+      if n == 2
+        if scores[index + 1] == 10
+          sum += scores[index + 2][0]
+        else
+          sum += scores[index + 1][1]
+        end
+      end
+    end
+  end
+
   return sum
 end
--- test/bowling_test.rb
+++ test/bowling_test.rb
@@ -6,4 +6,10 @@ class BowlingTest < Minitest::Test
     scores = [ [1, 3], [2, 6], [9, 0], [3, 5], [7, 2], [2, 2], [7, 1], [3, 4], [6, 3], [2, 6] ]
     assert_equal 74, bowling(scores)
   end
+
+  def test_bowling_2
+    # 1投目が0, スペア, ストライク追加
+    scores = [ [0, 3], [2, 6], [4, 6], [3, 5], [10, 0], [2, 2], [7, 1], [9, 1], [0, 0], [2, 6] ]
+    assert_equal 76, bowling(scores)
+  end
 end
